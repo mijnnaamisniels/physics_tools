79528c981f87517ea4aecbf985656e65c20f711d
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2019-09-21 11:50:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
   download_url = 'https://github.com/mijnnaamisniels/physics_tools/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['python', 'physics', 'tools'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
