c6d6008e6ada1bd6a11ea3fc61d304b10e810b42
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2019-10-01 10:23:48 +0200

    Update notation.py

--- physics_tools/notation.py
+++ physics_tools/notation.py
@@ -1,4 +1,4 @@
-﻿class not:
+﻿class notation:
     def SuperScript(number):
         number = str(number)
         return number.replace('0', '⁰').replace('1', '¹').replace('2', '²').replace('3', '³').replace('4', '⁴').replace('5', '⁵').replace('6', '⁶').replace('7', '⁷').replace('8', '⁸').replace('9', '⁹').replace('-', '⁻')
