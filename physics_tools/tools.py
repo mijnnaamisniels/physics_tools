674cb0e1b02d82641425d16ed191b7c70692d73c
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2019-10-01 20:26:47 +0200

    Update tools.py

--- physics_tools/tools.py
+++ physics_tools/tools.py
@@ -1,4 +1,4 @@
-﻿class notation:
+class notation:
     def SuperScript(number):
         number = str(number)
         return number.replace('0', '⁰').replace('1', '¹').replace('2', '²').replace('3', '³').replace('4', '⁴').replace('5', '⁵').replace('6', '⁶').replace('7', '⁷').replace('8', '⁸').replace('9', '⁹').replace('-', '⁻')
@@ -14,25 +14,37 @@
             return a + "·10" + notation.SuperScript(b)
 
 class var: 
+    '''Variabelen die handig zijn bij natuurkundige berekeningen'''
     #Massa's
-    m_e = 9.109*10**-31
-    m_p = 1.6726*10**-27
+    m_e = 9.10938*10**-31
+    m_p = 1.67262*10**-27
+    m_n = 1.67493*10**-27
     
-    
-    # Quantummechanica
+    #Overig
+    pi = 3.14159265359
     e = 1.602*10**-19
     c = 2.998*10**8
     h = 6.626*10**-34
+    h_str = h / 2*pi
     k = 1.381*10**-23
     e_0 = 8.854*10**-12
     mu_0 = 1.257*10**-6
     N_a = 6.022*10**23
     R = 8.314
-    
-    # Mechanica
     G = 6.674*10**-11
     g = 9.81
     
-    # Omzetten
+    
+    # Formules
     def ev_to_joule(ev):
         return ev*1.602*10**-19
+    
+    def joule_to_ev(joule):
+        return joule/(1.602*10**-19)
+    
+    def exp(number):
+        return 2.71828182846**number
+    
+    def root(number, macht=2):
+        return number**(1/macht)
+    
