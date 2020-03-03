564df7d0ae187799b3bc0c690f340fb600ce4b96
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2020-03-03 22:07:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Niels vd Nieuwendijk',                   # Type in your name
   author_email = 'niels.nieuwendijk@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/mijnnaamisniels',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mijnnaamisniels/physics_tools/archive/v_13.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mijnnaamisniels/physics_tools/archive/v13.tar.gz',    # I explain this later on
   keywords = ['python', 'physics', 'tools'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
