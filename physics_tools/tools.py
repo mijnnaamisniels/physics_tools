179853aed9bfbcc344b7ded99c8b1dab80bbc035
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2020-03-03 21:59:32 +0100

    Update tools.py

--- physics_tools/tools.py
+++ physics_tools/tools.py
@@ -4,7 +4,7 @@ def SuperScript(number):
     return number.replace('0', '⁰').replace('1', '¹').replace('2', '²').replace('3', '³').replace('4', '⁴').replace('5', '⁵').replace('6', '⁶').replace('7', '⁷').replace('8', '⁸').replace('9', '⁹').replace('-', '⁻')
 
 
-def sci_notation(number, num_significant, decimal=','):
+def sci_notation(number, num_significant=2, decimal=','):
     ret_string = "{0:.{1:d}e}".format(number, num_significant)
     a, b = ret_string.split("e")
     a = a.replace('.', decimal)
