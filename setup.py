0d529a56264e2c9299a0085f2b8cd66079dd5bb8
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2020-04-07 15:38:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'physics_tools',    
   packages = ['physics_tools'],   
-  version = '1.6',
+  version = '1.7',
   license='MIT',
   description = 'Tools for physics',
   author = 'Niels vd Nieuwendijk',
   author_email = 'niels.nieuwendijk@gmail.com', 
   url = 'https://github.com/user/mijnnaamisniels',
-  download_url = 'https://github.com/mijnnaamisniels/physics_tools/archive/v16.tar.gz',
+  download_url = 'https://github.com/mijnnaamisniels/physics_tools/archive/v17.tar.gz',
   keywords = ['python', 'physics', 'tools'], 
   install_requires=[
       ],
