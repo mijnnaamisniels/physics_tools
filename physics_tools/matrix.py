4880847f13b66d2dbff549d9dc16c5f8f50eb31f
mijnnaamisniels <48959623+mijnnaamisniels@users.noreply.github.com>
2020-04-07 14:19:35 +0200

    Update matrix.py

--- physics_tools/matrix.py
+++ physics_tools/matrix.py
@@ -1,6 +1,4 @@
-import numpy as np
 import sympy as sp
-import scipy as cp
 
 # Set sympy matrix display
 sp.init_printing(use_latex='mathjax') 
@@ -8,32 +6,45 @@ from IPython.display import display
 
 # Define 3d Vector function
 def identiteit():
-    return np.matrix([[1,0,0],[0,1,0],[0,0,1]])
+    return sp.Matrix([[1,0,0],[0,1,0],[0,0,1]])
 
 def schalen(x,y):
-    return np.matrix([[x,o,0],[0,y,0],[0,0,1]])
+    return sp.Matrix([[x,o,0],[0,y,0],[0,0,1]])
 
 def roteren(theta):
     theta = theta/180*np.pi
-    return np.matrix([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
+    return sp.Matrix([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
 
 def verplaatsen(x,y):
-    return np.matrix([[1, 0, x], [0, 1, y], [0, 0, 1]])
+    return sp.Matrix([[1, 0, x], [0, 1, y], [0, 0, 1]])
 
 def horizontaal_uitrekken(x):
-    return np.matrix([[1,x,0],[0,1,0],[0,0,1]])
+    return sp.Matrix([[1,x,0],[0,1,0],[0,0,1]])
 
 def verticaal_uitrekken(y):
-    return np.matrix([[1,0,0],[y,1,0],[0,0,1]])
+    return sp.Matrix([[1,0,0],[y,1,0],[0,0,1]])
 
 def spiegelx():
-    return np.matrix([[-1,0,0],[0,1,0],[0,0,1]])
+    return sp.Matrix([[-1,0,0],[0,1,0],[0,0,1]])
 
 def spiegely():
-    return np.matrix([[1,0,0],[0,-1,0],[0,0,1]])
+    return sp.Matrix([[1,0,0],[0,-1,0],[0,0,1]])
 
 def projecterenx():
-    return np.matrix([[1,0,0],[0,0,0],[0,0,1]])
+    return sp.Matrix([[1,0,0],[0,0,0],[0,0,1]])
 
 def projectereny():
-    return np.matrix([[0,0,0],[0,1,0],[0,0,1]])
+    return sp.Matrix([[0,0,0],[0,1,0],[0,0,1]])
+
+# Define optica matrixes
+def translatie(L):
+    return sp.Matrix([[1,L],[0,1]])
+
+def refractie(n,na,R):
+    return sp.Matrix([[1,0],[(n-na)/(R*na), n/na]])
+
+def sferische_spiegel(R):
+    return sp.Matrix([[1,0],[2/R,1]])
+
+def dunne_lens(f):
+    return sp.Matrix([[1,0],[-1/f,1]])
